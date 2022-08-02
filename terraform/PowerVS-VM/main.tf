42792e5f35c1f10f258ffd79145cde05f6918b65
Ben Stern <bstern@us.ibm.com>
2022-08-02 10:49:14 -0400

    commit

--- terraform/PowerVS-VM/main.tf
+++ terraform/PowerVS-VM/main.tf
@@ -3,12 +3,35 @@ provider "ibm" {
   zone      =   "lon04"
 }
 
+module "camtags" {
+  source = "../Modules/camtags"
+}
+
+variable "public_ssh_key" {
+  description = "Public SSH key used to connect to the virtual guest"
+}
+
+variable "datacenter" {
+  description = "Softlayer datacenter where infrastructure resources will be deployed"
+}
+
+variable "hostname" {
+  description = "Hostname of the virtual instance (small flavor) to be deployed"
+  default     = "debian-small"
+}
+
+variable "os_reference_code" {
+  type = "string"
+  description = "Operating system image id / template that should be used when creating the virtual image"
+  default = "DEBIAN_9_64"
+}
+  
 resource "ibm_pi_instance" "test-instance" {
     pi_memory             = "4"
     pi_processors         = "2"
     pi_instance_name      = "test-vm"
     pi_proc_type          = "shared"
-    pi_image_id           = "${data.ibm_pi_image.powerimages.id}"
+    pi_image_id           = "8810d643-5d62-46cb-bfc4-8c9593e4f7c0"
     pi_key_pair_name      = ibm_pi_key.key.key_id
     pi_sys_type           = "s922"
     pi_cloud_instance_id  = "51e1879c-bcbe-4ee1-a008-49cdba0eaf60"
