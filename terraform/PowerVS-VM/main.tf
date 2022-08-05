431b110121e03aa62362c85f4fef3219f746c442
Ben Stern <bstern@us.ibm.com>
2022-08-05 08:22:26 -0400

    Update main.tf

--- terraform/PowerVS-VM/main.tf
+++ terraform/PowerVS-VM/main.tf
@@ -8,7 +8,7 @@ resource "ibm_pi_instance" "test-instance" {
     pi_processors         = "2"
     pi_instance_name      = "test-vm"
     pi_proc_type          = "shared"
-    pi_image_id           = "${data.ibm_pi_image.powerimages.id}"
+    pi_image_id           = "8810d643-5d62-46cb-bfc4-8c9593e4f7c0"
     pi_key_pair_name      = ibm_pi_key.key.key_id
     pi_sys_type           = "s922"
     pi_cloud_instance_id  = "51e1879c-bcbe-4ee1-a008-49cdba0eaf60"
