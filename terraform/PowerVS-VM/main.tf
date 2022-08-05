ae03175ea873b452b8fe70a0bd30c245850bf202
Ben Stern <bstern@us.ibm.com>
2022-08-05 12:23:07 -0400

    Update main.tf

--- terraform/PowerVS-VM/main.tf
+++ terraform/PowerVS-VM/main.tf
@@ -1,5 +1,21 @@
 ## Template to be used by the IBM Provider for Power Systems
 
+terraform {
+  required_version = ">=1.0.0, <2.0"
+  required_providers {
+    ibm = {
+      source  = "IBM-Cloud/ibm"
+      version = ">= 1.44.0"
+    }
+  }
+}
+
+provider "ibm" {
+  ibmcloud_api_key = var.ibmcloud_api_key
+  region           = var.ibmcloud_region
+  zone             = "${var.ibmcloud_zone}"
+}
+
 data "ibm_pi_network" "power_networks" {
   count                = length(var.networks)
   pi_network_name      = var.networks[count.index]
